a607c15203af2ad567b98592994c8375d7df1994
Subrata Halder <subrata.haldar@codeclouds.com>
2025-06-22 17:04:12 +0530

    Added Servie  area

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import About from '../components/About'
+import Services from '../components/Services'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <About/>
+      <Services/>
     </div>
   )
 }
